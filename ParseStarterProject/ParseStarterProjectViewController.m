8ce1256eb06a10c466cf20920396035a7f0c0fd5
iOS Apps Development <ricardo@iosappsdev.org>
2014-06-10 22:29:02 -0700

    Cleaned Up Code

--- ParseStarterProject/ParseStarterProjectViewController.m
+++ ParseStarterProject/ParseStarterProjectViewController.m
@@ -11,25 +11,11 @@
 @implementation ParseStarterProjectViewController
 @synthesize firstName,lastName,email;
 
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-
 #pragma mark - UIViewController
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-}
-
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 - (IBAction)saveData:(id)sender {
@@ -40,7 +26,6 @@
     NSString *emailString = email.text;
     
     PFObject *userData = [PFObject objectWithClassName:@"userData"];
-    
     
     userData[@"firstName"] = firstNameString;
     userData[@"lastName"] = lastNameString;
@@ -53,21 +38,5 @@
             NSLog(@"Data saved successfully.");
         }
     }];
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
 }
 @end
